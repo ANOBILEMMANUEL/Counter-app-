ccf3db8cb18d8191791f58714ccea93b4e376e1c
Anobil Emmanuel <anobilemmnanuel2@gmail.com>
2025-02-15 17:38:25 -0800

    initial commit of the counter app

---
+++ script.js
@@ -0,0 +1,32 @@
+const incre=document.querySelector(".increase");
+const decre=document.querySelector(".decrease");
+const reset=document.querySelector(".reset");
+const dis =document.getElementById("display");
+const color =document.getElementById("color");
+const container =document.getElementsByClassName("container")[0];
+container.style.backgroundColor = "#0c3030"; 
+
+
+var count=0;
+incre.addEventListener("click",function(){
+    count++;
+    dis.innerHTML=count 
+});
+decre.addEventListener("click",()=>{
+    count--;
+    dis.innerHTML=count
+});
+reset.addEventListener("click",()=>{
+    dis.innerHTML="0"
+});
+color.addEventListener("input",()=>{
+    const picked =color.value;
+    container.style.backgroundColor = picked;   
+    if (picked.toLowerCase() === "#ffffff") {
+        alert("White color is not allowed. Please choose a different color.");
+       const cdefault=color.value = "#0c3030"; 
+        container.style.backgroundColor = cdefault; 
+    } else {
+        container.style.backgroundColor = picked; 
+    }
+});
